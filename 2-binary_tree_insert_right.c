a4b456355f8529ae01a4e9abda29fee6182533c9
akoredera <akoredera@gmail.com>
2024-03-27 04:55:58 -0700

    modified file

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,7 +9,7 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	if (parent != NULL && value >= parent->n)
+	if (parent != NULL)
 	{
 		binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
 
